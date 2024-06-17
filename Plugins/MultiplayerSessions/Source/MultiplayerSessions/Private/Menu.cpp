53bfd25de5a3a7d78e4defa07fdbc32fcdf1e25a
Moises Delgado <moises.f.delgado@gmail.com>
2024-06-17 16:45:00 -0400

    Adds button callbacks, and accessed the multiplayer sessions subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Menu.h"
+#include "Components/Button.h"
+#include "MultiplayerSessionsSubsystem.h"
 
 void UMenu::MenuSetup()
 {
@@ -21,5 +23,62 @@ void UMenu::MenuSetup()
             PlayerController->SetInputMode(InputModeData);
             PlayerController->SetShowMouseCursor(true);
         }
-    }    
+    }
+
+    UGameInstance* GameInstance = GetGameInstance();
+    if (GameInstance)
+    {
+        MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+    }
+}
+
+bool UMenu::Initialize()
+{
+    if (Super::Initialize() == false)
+    {
+        return false;
+    }
+
+    if (HostButton)
+    {
+        HostButton->OnClicked.AddDynamic(this, &UMenu::HostButtonClicked);
+    }
+
+    if (JoinButton)
+    {
+        JoinButton->OnClicked.AddDynamic(this, &UMenu::JoinButtonClicked);
+    }
+
+    return true;
+}
+
+void UMenu::HostButtonClicked()
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(
+            -1,
+            15.f,
+            FColor::Yellow,
+            FString(TEXT("Host Button Clicked"))
+        );
+    }
+
+    if (MultiplayerSessionsSubsystem)
+    {
+        MultiplayerSessionsSubsystem->CreateSession(4, FString("FreeForAll"));
+    }
+}
+
+void UMenu::JoinButtonClicked()
+{
+    if (GEngine)
+    {
+        GEngine->AddOnScreenDebugMessage(
+            -1,
+            15.f,
+            FColor::Yellow,
+            FString(TEXT("Join Button Clicked"))
+        );
+    }
 }
